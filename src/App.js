11083537db632a9f19cb5cd9592326e02dae7128
syk25 <syk2245@gmail.com>
2024-10-29 05:56:49 +0900

    coin tracker

--- src/App.js
+++ src/App.js
@@ -1,36 +1,34 @@
 import { useState, useEffect } from "react";
 
 function App() {
-    const [toDo, setToDo] = useState("");
-    const [toDos, setToDos] = useState([]);
-    const onChange = (event) => {
-        setToDo(event.target.value);
-    };
-    const onSubmit = (event) => {
-        event.preventDefault();
-        if (toDo === "") {
-            return;
-        }
-        setToDos((currentArray) => [toDo, ...currentArray]); // 스프레드문법 사용
-        setToDo("");
-    };
-    useEffect(() => console.log(toDos), [toDos]);
+    const [loading, setLoading] = useState();
+    const [coins, setCoins] = useState([]);
+    useEffect(() => {
+        fetch("https://api.coinpaprika.com/v1/tickers")
+            .then((response) => response.json())
+            .then((json) => {
+                setCoins(json);
+                setLoading(false);
+            });
+    }, []);
     return (
         <div>
-            <h1>My To Dos ({toDos.length})</h1>
-            <form onSubmit={onSubmit}>
-                <input
-                    value={toDo}
-                    onChange={onChange}
-                    type='text'
-                    placeholder='Write your todos'
-                />
-                <button>Add To Do</button>
-            </form>
+            <h1>The Coins!</h1>
+            {loading ? (
+                <strong>Loading...</strong>
+            ) : (
+                <strong>Total Coins:({coins.length})</strong>
+            )}
             <hr />
-            {toDos.map((item, index) => (
-                <li key={index}>{item}</li>
-            ))}
+            {loading ? null : (
+                <select>
+                    {coins.map((coin) => (
+                        <option>
+                            {coin.name} ({coin.symbol}): {coin.quotes.USD.price}
+                        </option>
+                    ))}
+                </select>
+            )}
         </div>
     );
 }
